e89a7f8e063e09487e71e9c4df7ef7ded3d389a5
Duy Pham <phamduy.uit@gmail.com>
2017-09-08 11:40:07 +0700

    Add and fix AlertUtils functions

--- androidutils/src/main/java/com/duyp/androidutils/AlertUtils.java
+++ androidutils/src/main/java/com/duyp/androidutils/AlertUtils.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
 import android.text.TextUtils;
@@ -12,73 +13,71 @@ import android.widget.Toast;
 
 
 /**
+ * Utilities functions for android alert and snack bar showing
  */
 public class AlertUtils {
 
     private static final String TAG = "AlertUtils";
 
-    public static void showConfirmDialog(Activity activity, String message, DialogInterface.OnClickListener positiveListener, DialogInterface.OnClickListener negativeListener) {
-        new AlertDialog.Builder(activity)
-                .setMessage(message)
-                .setPositiveButton(R.string.dialog_yes, positiveListener)
-                .setNegativeButton(R.string.dialog_no, negativeListener)
+    public static void showConfirmDialog(Context context, String message, DialogInterface.OnClickListener positiveListener) {
+        showConfirmDialog(context, null, message, positiveListener, null);
+    }
+
+    public static void showConfirmDialog(Context context, String message, DialogInterface.OnClickListener positiveListener,
+                                         DialogInterface.OnClickListener negativeListener) {
+        showConfirmDialog(context, null, message, positiveListener, negativeListener);
+    }
+
+    public static void showConfirmDialog(Context context, @Nullable String title, String message,
+                                         DialogInterface.OnClickListener positiveListener,
+                                         @Nullable DialogInterface.OnClickListener negativeListener) {
+        showAlertDialog(context, title, message, context.getString(R.string.dialog_yes), context.getString(R.string.dialog_no),
+                positiveListener, negativeListener);
+    }
+
+    public static void showAlertDialog(Context context, String title, String message, DialogInterface.OnClickListener listener) {
+        showAlertDialog(context, title, message, context.getString(R.string.dialog_ok), listener);
+    }
+
+    public static void showAlertDialog(Context context, String message, DialogInterface.OnClickListener listener) {
+        createAlertDialogBuilderInternal(context, message, context.getString(R.string.dialog_ok), listener)
                 .show();
     }
 
-    public static void showConfirmDialog(Activity activity, String title, String message, DialogInterface.OnClickListener listener) {
-        if (TextUtils.isEmpty(title)) {
-            title = "Confirm";
+    public static void showAlertDialog(Context context, @Nullable String title, String message,
+                                       String positive, DialogInterface.OnClickListener listener) {
+
+        AlertDialog.Builder builder = createAlertDialogBuilderInternal(context, message, positive, listener);
+        if (title != null) {
+            builder.setTitle(title);
         }
-        if (TextUtils.isEmpty(message)) {
-            message = "Are you sure?";
-        }
-        new AlertDialog.Builder(activity)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(R.string.dialog_yes, listener)
-                .setNegativeButton(R.string.dialog_no, null)
-                .show();
+        builder.show();
     }
 
-    public static void showConfirmDialogOne(Activity activity, String title, String message, DialogInterface.OnClickListener listener) {
-        new AlertDialog.Builder(activity)
-                .setTitle(title)
-                .setCancelable(false)
-                .setMessage(message)
-                .setPositiveButton(R.string.dialog_yes, listener)
-                .show();
-    }
-
-    public static void showAlertDialog(Context activity, String title, String message, DialogInterface.OnClickListener listener) {
-        new AlertDialog.Builder(activity)
-                .setCancelable(false)
-                .setTitle(title)
-                .setMessage(message)
-                .setPositiveButton(R.string.dialog_ok, listener)
-                .show();
-    }
-
-    public static void showAlertDialog(Context activity, String message, DialogInterface.OnClickListener listener) {
-        new AlertDialog.Builder(activity)
-                .setCancelable(false)
-                .setMessage(message)
-                .setPositiveButton(R.string.dialog_ok, listener)
-                .show();
-    }
-
-
-    public static void showAlertDialog(Context context, String title, String message, String positive, String negative,
+    public static void showAlertDialog(Context context, @Nullable String title, @NonNull String message,
+                                       @NonNull String positive,
+                                       @Nullable String negative,
                                        DialogInterface.OnClickListener positiveListener,
                                        DialogInterface.OnClickListener negativeListener) {
-        new AlertDialog.Builder(context)
-                .setTitle(title).setMessage(message)
-                .setPositiveButton(positive, positiveListener)
-                .setNegativeButton(negative, negativeListener)
-                .setCancelable(false)
-                .show();
-
-
+        AlertDialog.Builder builder = createAlertDialogBuilderInternal(context, message, positive, positiveListener);
+        if (title != null) {
+            builder.setTitle(title);
+        }
+        if (negative != null) {
+            builder.setNegativeButton(negative, negativeListener);
+        }
+        builder.show();
     }
+
+    private static AlertDialog.Builder createAlertDialogBuilderInternal(Context context,
+                                                                        @NonNull String message,
+                                                                        @NonNull String positiveButton,
+                                                                        DialogInterface.OnClickListener positiveListener) {
+        return new AlertDialog.Builder(context).setMessage(message)
+                .setPositiveButton(positiveButton, positiveListener)
+                .setCancelable(false);
+    }
+
 
     public static void showToastLongMessage(Context context, String message) {
         if (context != null)
