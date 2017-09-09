019053feffe70c4ca29b2dd5d3fb03f95129d832
Duy Pham <phamduy.uit@gmail.com>
2017-09-10 01:09:45 +0700

    Add square layout

---
+++ androidutils/src/main/java/com/duyp/androidutils/view/SquareLayout.java
@@ -0,0 +1,37 @@
+package com.duyp.androidutils.view;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+/**
+ * Created by Duy Pham on 3/3/17.
+ *
+ */
+public class SquareLayout extends FrameLayout {
+
+    public SquareLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public SquareLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        // Set a square layout.
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
