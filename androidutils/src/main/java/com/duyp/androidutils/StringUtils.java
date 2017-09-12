8a6beb8c6c087b7bc0b8adf1cadaec68ae7d4340
Duy Pham <phamduy.uit@gmail.com>
2017-09-12 11:04:34 +0700

    String utils - add isNotEmpty function

--- androidutils/src/main/java/com/duyp/androidutils/StringUtils.java
+++ androidutils/src/main/java/com/duyp/androidutils/StringUtils.java
@@ -1,15 +1,37 @@
 package com.duyp.androidutils;
 
+import android.text.TextUtils;
 import android.util.Base64;
 
 /**
  * Created by duypham on 9/7/17.
+ *
  */
 
 public class StringUtils {
 
+    /**
+     * Get Basic Auth token by given username and password
+     * @param username username
+     * @param password password
+     * @return {@link Base64} encoded auth token
+     */
     public static String getBasicAuth(String username, String password) {
         String credentials = username + ":" + password;
         return "Basic " + Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);
+    }
+
+    /**
+     * Check empty Strings
+     * @param strings input string array
+     * @return true if all element is NOT empty
+     */
+    public static boolean isNotEmpty(String... strings) {
+        for (String s : strings) {
+            if (TextUtils.isEmpty(s)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
