a0eab502617bf46f444682ee9ad59ac27b0968b1
Duy Pham <phamduy.uit@gmail.com>
2017-10-15 23:13:12 +0700

    fix error after modules changed

--- app/src/main/java/com/duyp/app/image/ImageDetailActivity.java
+++ app/src/main/java/com/duyp/app/image/ImageDetailActivity.java
@@ -1,20 +1,12 @@
 package com.duyp.app.image;
 
-import android.annotation.SuppressLint;
 import android.os.Build;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.widget.Toolbar;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.duyp.androidutils.image.glide.loader.GlideLoader;
-import com.duyp.androidutils.image.glide.loader.TransitionGlideLoader;
 import com.duyp.app.R;
 
 import static com.duyp.app.image.GlideTestActivity.*;
@@ -36,7 +28,7 @@ public class ImageDetailActivity extends AppCompatActivity {
             imageView.setTransitionName(TRANSITION_NAME_IMAGE);
         }
 
-        GlideLoader loader = new TransitionGlideLoader(this);
-        loader.loadImage(URL, imageView);
+//        GlideLoader loader = new TransitionGlideLoader(this);
+//        loader.loadImage(URL, imageView);
     }
 }
